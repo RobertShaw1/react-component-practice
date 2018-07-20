e4eb34ecebdc21145822afdf93a16f81e4e3fd64
Robert Shaw <r.shaw1@yahoo.com>
2018-07-19 21:24:18 -0500

    inverted button stories

--- components/Buttons/Button.jsx
+++ components/Buttons/Button.jsx
@@ -1,14 +1,33 @@
+/* eslint-disable no-confusing-arrow */
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+
+const defaultBackground = 'palevioletred';
+const defaultColor = 'white';
+
+const defaultColors = css`
+  background: ${defaultBackground};
+  color: ${defaultColor};
+`;
+
+const checkForInversion = css`
+  background: ${props => props.inverted ? 'transparent' : props.color};
+  color: ${props => props.inverted ? props.color : 'white'};
+`;
 
 export const Button = styled.button`
 /* Adapt the colours based on primary prop */
-background: ${props => props.primary ? 'palevioletred' : 'white'};
-color: ${props => props.primary ? 'white' : 'palevioletred'};
+${props => props.color ? checkForInversion : defaultColors};
 
 font-size: 1em;
 margin: 1em;
 padding: 0.25em 1em;
-border: 2px solid palevioletred;
+border: 2px solid ${props => props.color ? props.color : defaultColor};
 border-radius: 3px;
+
+/* Psuedo-classes */
+&:hover {
+  background: ${props => props.color && !props.inverted ? 'white' : props.color};
+  color: ${props => props.color && !props.inverted ? props.color : 'white'};
+}
 `;
--- stories/myStories/button.stories.js
+++ stories/myStories/button.stories.js
@@ -7,5 +7,34 @@ import { linkTo } from '@storybook/addon-links';
 import { Button } from '../../components/Buttons/Button';
 
 storiesOf('Button', module)
-  .add('with styled components', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-  .add('with primary prop', () => <Button primary onClick={action('clicked')}>Hello Button</Button>)
+  .add('with color inverted', () => (
+    <div>
+      <Button inverted color={'goldenrod'} onClick={action('clicked')}>Hello Button</Button>
+      <Button inverted color={'#00649f'} onClick={action('clicked')}>Hello Button</Button>
+      <Button inverted color={'#5d6ec7'} onClick={action('clicked')}>Hello Button</Button>
+      <Button inverted color={'#01aac1'} onClick={action('clicked')}>Hello Button</Button>
+      <Button inverted color={'palevioletred'} onClick={action('clicked')}>Hello Button</Button>
+      <Button inverted color={'orange'} onClick={action('clicked')}>Hello Button</Button>
+      <Button inverted color={'#456173'} onClick={action('clicked')}>Hello Button</Button>
+      {/* End Row 1 */}
+      <Button inverted color={'#499491'} onClick={action('clicked')}>Hello Button</Button>
+      <Button inverted color={'#528078'} onClick={action('clicked')}>Hello Button</Button>
+      <Button inverted color={'#375a7f'} onClick={action('clicked')}>Hello Button</Button>
+      <Button inverted color={'#4d7cae'} onClick={action('clicked')}>Hello Button</Button>
+      <Button inverted color={'#6a99cb'} onClick={action('clicked')}>Hello Button</Button>
+      <Button inverted color={'#2c3e50'} onClick={action('clicked')}>Hello Button</Button>
+      <Button inverted color={'#34495e'} onClick={action('clicked')}>Hello Button</Button>
+      {/* End Row 2 */}
+      <Button inverted color={'#cb007b'} onClick={action('clicked')}>Hello Button</Button>
+      <Button inverted color={'#986ead'} onClick={action('clicked')}>Hello Button</Button>
+      <Button inverted color={'#c83b3b'} onClick={action('clicked')}>Hello Button</Button>
+      <Button inverted color={'#ef9b59'} onClick={action('clicked')}>Hello Button</Button>
+      <Button inverted color={'#5727a3'} onClick={action('clicked')}>Hello Button</Button>
+      <Button inverted color={'#9153f4'} onClick={action('clicked')}>Hello Button</Button>
+      <Button inverted color={'#95956e'} onClick={action('clicked')}>Hello Button</Button>
+      {/* End 3 */}
+      <Button inverted color={'#c3a655'} onClick={action('clicked')}>Hello Button</Button>
+      <Button inverted color={'#9fa180'} onClick={action('clicked')}>Hello Button</Button>
+    </div>
+  ))
+  .add('with color', () => <Button color={'blue'} onClick={action('clicked')}>Hello Button</Button>)
