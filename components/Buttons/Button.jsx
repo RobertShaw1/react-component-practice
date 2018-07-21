4ced15bd1aaefff6fd741534591034f42c26abd8
Robert Shaw <r.shaw1@yahoo.com>
2018-07-20 21:17:40 -0500

    buttons w/ shadows

--- components/Buttons/Button.jsx
+++ components/Buttons/Button.jsx
@@ -19,15 +19,26 @@ export const Button = styled.button`
 /* Adapt the colours based on primary prop */
 ${props => props.color ? checkForInversion : defaultColors};
 
+height: 4em;
+width: 10em;
 font-size: 1em;
 margin: 1em;
 padding: 0.25em 1em;
 border: 2px solid ${props => props.color ? props.color : defaultColor};
 border-radius: 3px;
+outline: none;
+transition: 0.3s;
 
 /* Psuedo-classes */
 &:hover {
   background: ${props => props.color && !props.inverted ? 'white' : props.color};
   color: ${props => props.color && !props.inverted ? props.color : 'white'};
+  box-shadow: ${props => props.shadow ? '2px 8px 16px rgba(0, 0, 0, .5)' : 'none'};
+  transform: ${props => props.shadow ? 'translateY(-2px)' : 'none'};
+}
+&:active {
+  box-shadow: ${props => props.shadow ? '2px 4px 16px rgba(0, 0, 0, .5)' : 'none'};
+  transform: translateY(2px);
+  transition: 0s;
 }
 `;
--- stories/myStories/button.stories.js
+++ stories/myStories/button.stories.js
@@ -7,6 +7,7 @@ import { linkTo } from '@storybook/addon-links';
 import { Button } from '../../components/Buttons/Button';
 
 storiesOf('Button', module)
+  .add('with color', () => <Button color={'blue'} onClick={action('clicked')}>Hello Button</Button>)
   .add('with color inverted', () => (
     <div>
       <Button inverted color={'goldenrod'} onClick={action('clicked')}>Hello Button</Button>
@@ -37,4 +38,33 @@ storiesOf('Button', module)
       <Button inverted color={'#9fa180'} onClick={action('clicked')}>Hello Button</Button>
     </div>
   ))
-  .add('with color', () => <Button color={'blue'} onClick={action('clicked')}>Hello Button</Button>)
+  .add('with shadow', () => (
+      <div>
+        <Button inverted shadow color={'goldenrod'} onClick={action('clicked')}>Hello Button</Button>
+        <Button inverted shadow color={'#00649f'} onClick={action('clicked')}>Hello Button</Button>
+        <Button inverted shadow color={'#5d6ec7'} onClick={action('clicked')}>Hello Button</Button>
+        <Button inverted shadow color={'#01aac1'} onClick={action('clicked')}>Hello Button</Button>
+        <Button inverted shadow color={'palevioletred'} onClick={action('clicked')}>Hello Button</Button>
+        <Button inverted shadow color={'orange'} onClick={action('clicked')}>Hello Button</Button>
+        <Button inverted shadow color={'#456173'} onClick={action('clicked')}>Hello Button</Button>
+        {/* End Row 1 */}
+        <Button inverted shadow color={'#499491'} onClick={action('clicked')}>Hello Button</Button>
+        <Button inverted shadow color={'#528078'} onClick={action('clicked')}>Hello Button</Button>
+        <Button inverted shadow color={'#375a7f'} onClick={action('clicked')}>Hello Button</Button>
+        <Button inverted shadow color={'#4d7cae'} onClick={action('clicked')}>Hello Button</Button>
+        <Button inverted shadow color={'#6a99cb'} onClick={action('clicked')}>Hello Button</Button>
+        <Button inverted shadow color={'#2c3e50'} onClick={action('clicked')}>Hello Button</Button>
+        <Button inverted shadow color={'#34495e'} onClick={action('clicked')}>Hello Button</Button>
+        {/* End Row 2 */}
+        <Button inverted shadow color={'#cb007b'} onClick={action('clicked')}>Hello Button</Button>
+        <Button inverted shadow color={'#986ead'} onClick={action('clicked')}>Hello Button</Button>
+        <Button inverted shadow color={'#c83b3b'} onClick={action('clicked')}>Hello Button</Button>
+        <Button inverted shadow color={'#ef9b59'} onClick={action('clicked')}>Hello Button</Button>
+        <Button inverted shadow color={'#5727a3'} onClick={action('clicked')}>Hello Button</Button>
+        <Button inverted shadow color={'#9153f4'} onClick={action('clicked')}>Hello Button</Button>
+        <Button inverted shadow color={'#95956e'} onClick={action('clicked')}>Hello Button</Button>
+        {/* End 3 */}
+        <Button inverted shadow color={'#c3a655'} onClick={action('clicked')}>Hello Button</Button>
+        <Button inverted shadow color={'#9fa180'} onClick={action('clicked')}>Hello Button</Button>
+      </div>
+    ))
