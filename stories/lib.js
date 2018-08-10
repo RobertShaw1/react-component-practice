93523df9c5a0aefb81a5b2f1e4e1be9322caa465
Robert Shaw <r.shaw1@yahoo.com>
2018-08-09 19:58:16 -0500

    storybook addons

---
+++ stories/lib.js
@@ -0,0 +1,6 @@
+import addonBackgrounds from '@storybook/addon-backgrounds';
+
+export const backgrounds = addonBackgrounds([
+  { name: 'twitter', value: '#00aced', default: true },
+  { name: 'facebook', value: '#3b5998' },
+]);
